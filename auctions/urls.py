443c99f3bad0dc33132ed063ed63bbdb5b891cc8
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-08-03 23:29:58 +0530

    Updating urls.py for categories

--- auctions/urls.py
+++ auctions/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
     path("categories", views.categories, name="categories"),
-    path("categories/memes", views.cat_memes, name="cat_memes")
+    path("categories/memes", views.cat_memes, name="cat_memes"),
+    path("categories/gpus", views.cat_gpus, name="cat_gpus"),
+    path("categories/proc", views.cat_proc, name="cat_proc"),
+    path("categories/ram", views.cat_ram, name="cat_ram"),
+    path("categories/monitor", views.cat_monitor, name="cat_monitor")
 ]
