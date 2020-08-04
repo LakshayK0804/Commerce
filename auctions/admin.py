c07841aeaff1b903edeca041f13a7da540711829
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-08-04 23:57:55 +0530

    Adding fields

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import Snippet
+from django.contrib.auth.models import Group
 
 # Register your models here.
 
-admin.site.site_header = 'Admin Tutorial DashBoard'
+admin.site.site_header = 'Admin DashBoard'
+
+class SnippetAdmin(admin.ModelAdmin):
+    fields = ('title','radio')
+
 admin.site.register(Snippet)
+admin.site.unregister(Group)
