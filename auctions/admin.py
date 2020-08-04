3e1db9eca835576bdbd13f14e67aa8b5a6f2484a
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-08-04 23:59:31 +0530

    Update admin.py

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import Snippet
-from django.contrib.auth.models import Group
+
 
 # Register your models here.
 
@@ -9,5 +9,4 @@ admin.site.site_header = 'Admin DashBoard'
 class SnippetAdmin(admin.ModelAdmin):
     fields = ('title','radio')
 
-admin.site.register(Snippet)
-admin.site.unregister(Group)
+admin.site.register(Snippet, SnippetAdmin)
