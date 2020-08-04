26797a3da0c397a3ba74ee5aa5cb122b851dbcb7
LAKSHAYK0804 <spambotofficial44@gmail.com>
2020-08-04 23:45:54 +0530

    Admin Controls

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Snippet
 
 # Register your models here.
+
+admin.site.site_header = 'Admin Tutorial DashBoard'
+admin.site.register(Snippet)
--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Snippet(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    def body_preview(self):
+        return self.body[:50]
